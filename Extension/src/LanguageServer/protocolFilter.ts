065a8375d06ca3296f582bdd95b23bb91f2c9786
Colen Garoutte-Carson <coleng@microsoft.com>
2022-12-20 15:58:30 -0800

    Use awaits and latest languageserver client module

--- Extension/src/LanguageServer/protocolFilter.ts
+++ Extension/src/LanguageServer/protocolFilter.ts
@@ -10,20 +10,17 @@ import { Client } from './client';
 import * as vscode from 'vscode';
 import { CppSettings } from './settings';
 import { clients, onDidChangeActiveTextEditor } from './extension';
-import { getOutputChannelLogger, Logger } from '../logger';
 
 export function createProtocolFilter(): Middleware {
     // Disabling lint for invoke handlers
-    const defaultHandler: (data: any, callback: (data: any) => Promise<void>) => Promise<void> = async (data, callback: (data: any) => void) => { clients.ActiveClient.notifyWhenLanguageClientReady(() => callback(data)); };
-    // const invoke1 = async (a: any, next: (a: any) => any) => { await clients.ActiveClient.awaitUntilLanguageClientReady(); return next(a); };
+    const invoke1 = async (a: any, next: (a: any) => any) => { await clients.ActiveClient.awaitUntilLanguageClientReady(); return next(a); };
     const invoke2 = async (a: any, b: any, next: (a: any, b: any) => any) => { await clients.ActiveClient.awaitUntilLanguageClientReady(); return next(a, b); };
     const invoke3 = async (a: any, b: any, c: any, next: (a: any, b: any, c: any) => any) => { await clients.ActiveClient.awaitUntilLanguageClientReady(); return next(a, b, c); };
-    const invoke4 = async (a: any, b: any, c: any, d: any, next: (a: any, b: any, c: any, d: any) => any) => { await clients.ActiveClient.awaitUntilLanguageClientReady(); return next(a, b, c, d); };
-    // const invoke5 = async (a: any, b: any, c: any, d: any, e: any, next: (a: any, b: any, c: any, d: any, e: any) => any) => { await clients.ActiveClient.awaitUntilLanguageClientReady(); return next(a, b, c, d, e); };
-    /* tslint:enable */
+    const invoke4 = async (a: any, b: any, c: any, d: any, next: (a: any, b: any, c: any, d: any) => any) => { await clients.ActiveClient.awaitUntilLanguageClientReady(); return next(a, b, c, d); };    /* tslint:enable */
 
     return {
         didOpen: async (document, sendMessage) => {
+            await clients.ActiveClient.awaitUntilLanguageClientReady();
             const editor: vscode.TextEditor | undefined = vscode.window.visibleTextEditors.find(e => e.document === document);
             if (editor) {
                 // If the file was visible editor when we were activated, we will not get a call to
@@ -35,11 +32,9 @@ export function createProtocolFilter(): Middleware {
                     clients.timeTelemetryCollector.setDidOpenTime(document.uri);
                     if (clients.checkOwnership(me, document)) {
                         me.TrackedDocuments.add(document);
-                        const finishDidOpen = (doc: vscode.TextDocument) => {
-                            me.provideCustomConfiguration(doc.uri, undefined);
-                            const out: Logger = getOutputChannelLogger();
-                            out.appendLine("protocolFilter - sending didOpen message via sendMessage(doc)");
-                            sendMessage(doc);
+                        const finishDidOpen = async (doc: vscode.TextDocument) => {
+                            await me.provideCustomConfiguration(doc.uri, undefined);
+                            await sendMessage(doc);
                             me.onDidOpenTextDocument(doc);
                             if (editor && editor === vscode.window.activeTextEditor) {
                                 onDidChangeActiveTextEditor(editor);
@@ -53,14 +48,13 @@ export function createProtocolFilter(): Middleware {
                                 const mappingString: string = fileName + "@" + document.uri.fsPath;
                                 me.addFileAssociations(mappingString, "cpp");
                                 me.sendDidChangeSettings();
-                                vscode.languages.setTextDocumentLanguage(document, "cpp").then((newDoc: vscode.TextDocument) => {
-                                    finishDidOpen(newDoc);
-                                });
+                                const newDoc: vscode.TextDocument = await vscode.languages.setTextDocumentLanguage(document, "cpp");
+                                await finishDidOpen(newDoc);
                                 languageChanged = true;
                             }
                         }
                         if (!languageChanged) {
-                            finishDidOpen(document);
+                            await finishDidOpen(document);
                         }
                     }
                 }
@@ -75,42 +69,39 @@ export function createProtocolFilter(): Middleware {
             }
         },
         didChange: async (textDocumentChangeEvent, sendMessage) => {
+            await clients.ActiveClient.awaitUntilLanguageClientReady();
             const me: Client = clients.getClientFor(textDocumentChangeEvent.document.uri);
-            if (!me.TrackedDocuments.has(textDocumentChangeEvent.document)) {
-                console.log("!!!!!!!!!!!!!!!!!!!!!! We shouldn't get here.  didChange should not occur until didOpen has been processed.");
-                // processDelayedDidOpen(textDocumentChangeEvent.document);
-            }
             me.onDidChangeTextDocument(textDocumentChangeEvent);
-            sendMessage(textDocumentChangeEvent);
-            // me.notifyWhenLanguageClientReady(() => sendMessage(textDocumentChangeEvent));
+            await sendMessage(textDocumentChangeEvent);
         },
-        willSave: defaultHandler,
+        willSave: invoke1,
         willSaveWaitUntil: async (event, sendMessage) => {
+            // await clients.ActiveClient.awaitUntilLanguageClientReady();
+            // Don't use awaitUntilLanguageClientReady.
+            // Otherwise, the message can be delayed too long.
             const me: Client = clients.getClientFor(event.document.uri);
             if (me.TrackedDocuments.has(event.document)) {
-                // Don't use me.requestWhenReady or notifyWhenLanguageClientReady;
-                // otherwise, the message can be delayed too long.
                 return sendMessage(event);
             }
-            return Promise.resolve([]);
+            return [];
         },
-        didSave: defaultHandler,
+        didSave: invoke1,
         didClose: async (document, sendMessage) => {
+            await clients.ActiveClient.awaitUntilLanguageClientReady();
             const me: Client = clients.getClientFor(document.uri);
             if (me.TrackedDocuments.has(document)) {
                 me.onDidCloseTextDocument(document);
                 me.TrackedDocuments.delete(document);
-                sendMessage(document);
-                // me.notifyWhenLanguageClientReady(() => sendMessage(document));
+                await sendMessage(document);
             }
         },
-
         provideCompletionItem: invoke4,
         resolveCompletionItem: invoke2,
-        provideHover: (document, position, token, next: (document: any, position: any, token: any) => any) => {
+        provideHover: async (document, position, token, next: (document: any, position: any, token: any) => any) => {
+            await clients.ActiveClient.awaitUntilLanguageClientReady();
             const me: Client = clients.getClientFor(document.uri);
-            if (clients.checkOwnership(me, document)) {
-                return clients.ActiveClient.requestWhenReady(() => next(document, position, token));
+            if (me.TrackedDocuments.has(document)) {
+                return next(document, position, token);
             }
             return null;
         },
@@ -118,10 +109,9 @@ export function createProtocolFilter(): Middleware {
         provideDefinition: invoke3,
         provideReferences: invoke4,
         provideDocumentHighlights: invoke3,
-        provideDeclaration: invoke3
-        // I believe the default handler will do the same thing.
-        // workspace: {
-        //     didChangeConfiguration: (sections, sendMessage) => sendMessage(sections)
-        // }
+        provideDeclaration: invoke3,
+        workspace: {
+            didChangeConfiguration: invoke1
+        }
     };
 }
